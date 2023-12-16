3401a31efd8ff3d6d6badd1a84a82130767225d1
Guy0851 <delcourt.guillaume08@gmail.com>
2023-12-16 01:00:44 +0100

    Migration de portable vers fixe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './don-sang.png';
 import './App.css';
 
 function App() {
@@ -7,17 +7,22 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Plateforme Mon Don
         </p>
+      </header>
+      <body>
+        
+      </body>
+      <footer>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          (doc officielle react)
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
